4b60e6d68017aefa7150d5c6f49e539cec0714ea
Elkin Humberto Chaparro Franklin <elkinchaparro@gmail.com>
2024-11-14 00:22:43 -0500

    first commit

---
+++ CLASE02/HolaMundo.java
@@ -0,0 +1,14 @@
+package CLASE02;
+import java.util.Scanner;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo!");
+        var entrada = new Scanner(System.in);
+        System.out.print("Ingrese un numero entero: ");
+        var entero = entrada.nextInt();
+        System.out.println("El numero actual ingresado es: "+entero);
+        entrada.close();
+    }
+}
