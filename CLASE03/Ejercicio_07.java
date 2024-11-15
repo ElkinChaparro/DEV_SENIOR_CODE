8859df96f2ec69791334fb04a29b873e741a84e0
Elkin Humberto Chaparro Franklin <elkinchaparro@gmail.com>
2024-11-15 02:42:16 -0500

    Ejercicios

---
+++ CLASE03/Ejercicio_07.java
@@ -0,0 +1,9 @@
+/*## Ejercicio 7: Calculadora Básica
+Escribe un programa que solicite al usuario dos números
+y luego realice las operaciones de suma, resta, multiplicación
+y división con esos números. Muestra los resultados en la consola.*/
+public class Ejercicio_07 {
+    public static void main(String[] args) {
+        
+    }
+}
