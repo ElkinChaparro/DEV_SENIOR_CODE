86870dc6059acac8c9717fa4e42fbfc0f808ab14
ElkinChaparro <elkinchaparro@gmail.com>
2025-06-19 19:56:55 -0500

    Ejercicios

---
+++ MODULO04/CLASE01/Ejercicios/E02/verificacion_duplicados/src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package MODULO04.CLASE01.Ejercicios.E02.verificacion_duplicados.src.main.java.com.example;
+
+/* ## Ejercicio 2 - Verificación de duplicados en una lista de
+correos electrónicos.
+Escribe un programa que verifique si hay correos electrónicos
+duplicados en una lista. El programa debe leer una lista de
+correos electrónicos y utilizar una estructura de datos adecuada
+para identificar y reportar cualquier duplicado encontrado.*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
